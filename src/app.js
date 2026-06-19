eb89f361469e1f69f358952c048339cdb4b5804c
Aryan Meena <songrimleader@gmail.com>
2026-06-19 14:34:41 +0530

    feat: Serve android/download statically

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/downloads", express.static(path.join(__dirname, "../downloads")));
+app.use("/android/download", express.static(path.join(__dirname, "../android/download")));
 
 app.use("/api/meta", metaRoutes);
 app.use("/api", apiRouter);
